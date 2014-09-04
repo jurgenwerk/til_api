b3105ec4ddedc2dc720fdbf2459d2b76f6ff08e4
jurgen <matic@jurglic.si>
2014-09-04 22:56:21 +0200

    add mongo stuff

--- config/application.rb
+++ config/application.rb
@@ -1,11 +1,12 @@
 require File.expand_path('../boot', __FILE__)
 
 require "action_controller/railtie"
+require 'rails/mongoid'
 
 Bundler.require(*Rails.groups)
 
 module TilApi
   class Application < Rails::Application
-    
+
   end
 end
